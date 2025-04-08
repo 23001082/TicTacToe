b6467878351cf3a595e0e476833f14693eb03c11
23001082 <163306416+23001082@users.noreply.github.com>
2025-04-08 11:43:19 +0200

    Add files via upload

---
+++ build/ElectionStaticcs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.electionstaticcs;
+
+/**
+ *
+ * @author 23001082
+ */
+public class ElectionStaticcs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
